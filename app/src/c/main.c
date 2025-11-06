5965f4e1f23cac700272d712ff1b3be509341c7b
Pavel Škoda <p@v3l.cz>
2025-11-06 13:49:32 +0000

    Refactor click handler functions and improve accelerometer handling

--- app/src/c/main.c
+++ app/src/c/main.c
@@ -3,25 +3,42 @@
 static Window *s_window;
 static TextLayer *s_text_layer;
 
-static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void _handle_click_select(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Select");
 }
 
-static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void _handle_click_up(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Up");
 }
 
-static void prv_down_click_handler(ClickRecognizerRef recognizer, void *context) {
+static void _handle_click_down(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(s_text_layer, "Down");
 }
 
-static void prv_click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, prv_select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
+static void _handle_accelerometer(AccelData *data, uint32_t num_samples) {
+  static int16_t x = 0, y = 0, z = 0;
+  for (uint32_t i = 0; i < num_samples; i++) {
+    x += data[i].x;
+    y += data[i].y;
+    z += data[i].z;
+  }
+  x /= num_samples;
+  y /= num_samples;
+  z /= num_samples;
+  if (x > 2000 || x < -2000 || y > 2000 || y < -2000 || z > 2000 || z < -2000) {
+    text_layer_set_text(s_text_layer, "Shake detected!");
+  }
 }
 
-static void prv_window_load(Window *window) {
+static void _click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, _handle_click_select);
+  window_single_click_subscribe(BUTTON_ID_UP, _handle_click_up);
+  window_single_click_subscribe(BUTTON_ID_DOWN, _handle_click_down);
+  accel_data_service_subscribe(5, _handle_accelerometer);
+  accel_service_set_sampling_rate(ACCEL_SAMPLING_100HZ);
+}
+
+static void _window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
@@ -31,30 +48,28 @@ static void prv_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 }
 
-static void prv_window_unload(Window *window) {
+static void _window_unload(Window *window) {
   text_layer_destroy(s_text_layer);
 }
 
-static void prv_init(void) {
+static void _init(void) {
   s_window = window_create();
-  window_set_click_config_provider(s_window, prv_click_config_provider);
+  window_set_click_config_provider(s_window, _click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
-    .load = prv_window_load,
-    .unload = prv_window_unload,
+    .load = _window_load,
+    .unload = _window_unload
+  ,
   });
   const bool animated = true;
   window_stack_push(s_window, animated);
 }
 
-static void prv_deinit(void) {
+static void _destroy(void) {
   window_destroy(s_window);
 }
 
 int main(void) {
-  prv_init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", s_window);
-
+  _init();
   app_event_loop();
-  prv_deinit();
+  _destroy();
 }
